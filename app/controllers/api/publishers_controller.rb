fa483c3e4cd813321a260c6ba8d955f392e4e6c1
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-07-02 12:00:24 -0700

    Refactor publishers controller

--- app/controllers/api/publishers_controller.rb
+++ app/controllers/api/publishers_controller.rb
@@ -2,10 +2,17 @@ require 'byebug'
 
 class Api::PublishersController < ApplicationController
   def index
-    # publishers = 'Popular_Science|PC_Magazine|TechCrunch|Gizmodo|The_Verge|GeekWire'
-    publishers = 'Popular_Science'
-    response = HTTParty.get("https://en.wikipedia.org/w/api.php?action=query&titles=#{publishers}&prop=revisions&rvprop=content&format=json")
-    result = find_values(response)
+    publishers = 'Popular_Science|PC_Magazine|TechCrunch|Gizmodo|The_Verge|GeekWire'
+    # publishers = 'Popular_Science'
+    url = "https://en.wikipedia.org/w/api.php?action=query&titles=#{publishers}\
+    &prop=revisions&rvprop=content&format=json"
+
+    response = HTTParty.get(url)
+
+    @items = ['logo', 'image_file', 'type', 'editor', 'owner', 'url', 'website',
+    'launch date', 'launch_date', 'firstdate', 'author', 'founded']
+
+    @result = find_values(response.parsed_response)
   end
 
   private
@@ -13,56 +20,68 @@ class Api::PublishersController < ApplicationController
   def find_values(response)
     final_result = []
 
-    items = ['logo', 'image_file', 'type', 'editor', 'owner', 'url', 'website',
-      'launch date', 'launch_date', 'firstdate', 'author', 'founded']
     pages = response['query']['pages'].keys
 
     pages.each do |page|
-      values = {}
-      info_array = response['query']['pages'][page]['revisions'][0]['*'].split("|")
-      values['name'] = response['query']['pages'][page]['title']
+      @values = {}
+      info_array = response['query']['pages'][page]['revisions'][0]\
+      ['*'].split("|")
+      @values['name'] = response['query']['pages'][page]['title']
       idx = 0
-      while values.keys.length < 8 && idx < 40
-        items.each do |item|
+      while @values.keys.length < 8 && idx < 40
+        @items.each do |item|
           if info_array[idx][0...item.length + 1].include?(item)
-            if item == 'logo' || item == 'image_file'
-              if !values.key?('logo') && (info_array[idx].include?('.png') || info_array[idx].include?('.svg'))
-                image_dir = parse_value(info_array[idx], item)
-                name = values['name'].gsub(" ", "_")
-                values['logo'] = "https://en.wikipedia.org/wiki/#{name}#/media/#{image_dir}"
-              end
-            elsif item == 'type'
-              if !info_array[idx + 1].include?("=")
-                has_equal_sign = false
-                sub_idx = idx + 1
-              else
-                sub_idx = idx
-                has_equal_sign = true
-              end
-              values[item] = parse_value(info_array[sub_idx], item, has_equal_sign)
-            elsif item == 'url' || item == 'website'
-              if info_array[idx].include?("URL")
-                values['website'] = parse_value(info_array[idx + 1], 'url', false)
-              else
-                equals_idx = info_array[idx].index("=")
-                if info_array[idx][equals_idx + 1] == "["
-                  values['website'] = parse_value(info_array[idx], 'url')
-                end
-              end
-            elsif item == 'launch date' || item == 'launch_date' || item == 'firstdate' || item == 'founded'
-              values['launch_date'] = determine_launch_date(info_array, idx)
-            elsif item == 'editor' || item == 'owner' || item == 'url'
-              values[item] = parse_value(info_array[idx], item)
+            case item
+            when 'logo', 'image_file'
+              determine_logo(info_array, idx, item)
+            when 'type'
+              determine_type(info_array, idx)
+            when 'url', 'website'
+              determine_website(info_array, idx)
+            when 'launch_date', "launch date", "firstdate", "founded"
+              determine_launch_date(info_array, idx)
+            when 'editor', 'owner', 'author'
+              @values[item] = parse_value(info_array[idx], item)
             end
           end
         end
-        p values
         idx += 1
       end
-      final_result << values
+      final_result << @values
     end
-    debugger
     final_result
+  end
+
+  def determine_logo(info_array, idx, item)
+    if !@values.key?('logo') && (info_array[idx].include?('.png') ||
+      info_array[idx].include?('.svg'))
+      image_dir = parse_value(info_array[idx], item)
+      name = @values['name'].gsub(" ", "_")
+      @values['logo'] =
+        "https://en.wikipedia.org/wiki/#{name}#/media/#{image_dir}"
+    end
+  end
+
+  def determine_type(info_array, idx)
+    if !info_array[idx + 1].include?("=")
+      has_equal_sign = false
+      sub_idx = idx + 1
+    else
+      sub_idx = idx
+      has_equal_sign = true
+    end
+    @values['type'] = parse_value(info_array[sub_idx], 'type', has_equal_sign)
+  end
+
+  def determine_website(info_array, idx)
+    if info_array[idx].include?("URL")
+      @values['website'] = parse_value(info_array[idx + 1], 'url', false)
+    else
+      equals_idx = info_array[idx].index("=")
+      if info_array[idx][equals_idx + 1] == "["
+        @values['website'] = parse_value(info_array[idx], 'url')
+      end
+    end
   end
 
   def determine_launch_date(info_array, idx)
@@ -83,27 +102,33 @@ class Api::PublishersController < ApplicationController
       end
       count += 1
     end
-    date
+    @values['launch_date'] = date
   end
 
   def parse_value(string, type, has_equal_sign = true)
-    string.gsub!("\n", "")
-    string.delete!("[]}")
+    parsed_result = string.gsub("\n", "")
+    parsed_result.delete!("[]}")
     if has_equal_sign
-      string = string.split("=")[1]
-      string = string[1..-1] if string[0] == " "
+      parsed_result = parsed_result.split("=")[1]
+      parsed_result = parsed_result[1..-1] if parsed_result[0] == " "
 
-      if type == 'logo' || type == 'image_file'
-        string.gsub!(" ", "_")
+      case type
+      when 'logo', 'image_file'
+        parsed_result.gsub!(" ", "_")
         if type == 'image_file'
-          string = "File:#{string}"
+          parsed_result = "File:#{parsed_result}"
         end
-      elsif type == 'owner'
-        string = string.split("<br>")
-      elsif type == 'url'
-        string = string.split(" ")[0]
+      when 'owner'
+        parsed_result = parsed_result.split("<br>")
+      when 'url'
+        parsed_result = parsed_result.split(" ")[0]
+      when 'author'
+        parsed_result = parsed_result.split("<br />")
+        if parsed_result.length > 1
+          parsed_result[1] = parsed_result[1].split("<")[0]
+        end
       end
     end
-    string
+    parsed_result
   end
 end
